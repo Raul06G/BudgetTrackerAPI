5ab49c92e3e445ebcbb9cd7ae4683956208d1055
Andre Hansen <andre.hansen146@gmail.com>
2024-04-29 11:02:31 +0200

    gjore endringeg

--- api/index.py
+++ api/index.py
@@ -9,6 +9,31 @@ passord = "hei123"
 def default():
     return 'Velkomen til vårt nydelijge api'
 
+@app.route('/loginpage')
+def loginpage():
+    return '''
+        <h1>velkomen til login</h1>
+        <form>
+            <input>Brukernavn</input> <br/>
+            <input>Passord</input> <br />
+            <button onclick="hei()"><a href="result">hei</a></button>
+            <button onclick="hei()">Hei</button>
+        </form>
+
+        <script>
+            funtion hei() {
+                //login
+                console.log("hello world);
+                //window.location.href = "https://vg.no";
+            }
+        </script>
+    '''
+
+@app.route('/result')
+def ressult():
+    return '''<h1>loged in</h1>'''
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
