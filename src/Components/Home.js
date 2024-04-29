a16fb529b7035dc07da41e088d19f731d0c5451f
AFS0023747 <AFS0023747@agderskolen.no>
2024-04-29 11:40:11 +0200

    La til filer som jeg gjorde

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import navbar from './navbar'
+
+const home = () => {
+  return (
+    <div className='home-container'>
+        {/* <navbar/> */}
+        <h1>hei</h1>
+    </div>
+  )
+}
+
+export default home;
